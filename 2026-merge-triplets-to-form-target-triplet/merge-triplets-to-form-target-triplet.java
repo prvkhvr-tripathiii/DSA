a6ebde0ab767a88f34365739e0b574fb2fe175a5
Prakhar Tripathi <116378378+prvkhvr-tripathiii@users.noreply.github.com>
2026-09-15 00:10:10 +0530

    Time: 6 ms (44.76%) | Memory: 168.5 MB (80.53%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -1,27 +1,23 @@
 class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
-        ArrayList <int[]> list = new ArrayList<>();
-        for (int i = 0; i < triplets.length; i++) {
-            if (triplets[i][0] <= target[0]
-            && triplets[i][1] <= target[1]
-            && triplets[i][2] <= target[2]) {
-                list.add(triplets[i]);
-            }
-        }
 
         boolean aFound = false;
         boolean bFound = false;
         boolean cFound = false;
 
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i)[0] == target[0]) {
-                aFound = true;
-            }
-            if (list.get(i)[1] == target[1]) {
-                bFound = true;
-            }
-            if (list.get(i)[2] == target[2]) {
-                cFound = true;
+        for (int i = 0; i < triplets.length; i++) {
+            if (triplets[i][0] <= target[0]
+            && triplets[i][1] <= target[1]
+            && triplets[i][2] <= target[2]) {
+                if (triplets[i][0] == target[0]){
+                    aFound = true;
+                }
+                if (triplets[i][1] == target[1]){
+                    bFound = true;
+                }
+                if (triplets[i][2] == target[2]){
+                    cFound = true;
+                }
             }
         }
 
