fe2373512ddc4462ccfac9e7c685d3438814f1fc
Prakhar Tripathi <116378378+prvkhvr-tripathiii@users.noreply.github.com>
2026-09-15 08:21:41 +0530

    Time: 2 ms (97.48%) | Memory: 72.7 MB (6.01%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean lemonadeChange(int[] bills) {
         int count5 = 0;
         int count10 = 0;
-        int count20 = 0;
         int totalMoney = 0;
 
         for (int i = 0; i < bills.length; i++) {
@@ -11,23 +10,19 @@ class Solution {
                 count5++;
             }
             else if (bills[i] == 10) {
-                if (count5 > 0) {
-                    count5--;
-                    count10++;
-                } 
-                else {
+                if (count5 == 0) {
                     return false;
-                }
+                } 
+                count5--;
+                count10++;
             }
             else {
                 if (count5 > 0 && count10 > 0) {
                     count5--;
                     count10--;
-                    count20++;
                 } 
                 else if (count5 > 2) {
                     count5 -= 3;
-                    count20++;
                 }
                 else {
                     return false;
