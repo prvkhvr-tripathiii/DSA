c1271a06addd80f94271c19659972126a0d7b848
Prakhar Tripathi <116378378+prvkhvr-tripathiii@users.noreply.github.com>
2026-09-26 05:47:43 +0530

    Time: 12 ms (44.02%) | Memory: 48 MB (6.88%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -2,27 +2,48 @@ class Solution {
     public String[] findRelativeRanks(int[] score) {
         HashMap <Integer, Integer> map = new HashMap<>();
         String rank[] = new String[score.length];
+        int n = score.length - 1;
 
         for (int i = 0; i < score.length; i++) {
             map.put(score[i], i);
         }
 
-        for (int i = 0; i < score.length; i++) {
-            int highest = Collections.max(map.keySet());
-            rank[map.remove(highest)] = String.valueOf(i + 1);
+        Arrays.sort(score);
+        // rank[map.get(score[n])] = "Gold Medal";
+        // rank[map.get(score[n - 1])] = "Silver Medal";
+        // rank[map.get(score[n - 2])] = "Bronze Medal";
+
+        for (int i = n; i >= 0; i--) {
+            if (i == n) {
+                rank[map.get(score[n])] = "Gold Medal";
+            }
+            else if (i == n - 1) {
+                rank[map.get(score[n - 1])] = "Silver Medal";
+            }
+            else if (i == n - 2) {
+                rank[map.get(score[n - 2])] = "Bronze Medal";
+            } 
+            else {
+                rank[map.get(score[i])] = String.valueOf(n - i + 1);
+            }
         }
 
-        for (int i = 0; i < rank.length; i++) {
-            if ("1".equals(rank[i])) {
-                rank[i] = "Gold Medal";
-            }
-            if ("2".equals(rank[i])) {
-                rank[i] = "Silver Medal";
-            }
-            if ("3".equals(rank[i])) {
-                rank[i] = "Bronze Medal";
-            }
-        }
+        // for (int i = 0; i < score.length; i++) {
+        //     int highest = Collections.max(map.keySet());
+        //     rank[map.remove(highest)] = String.valueOf(i + 1);
+        // }
+
+        // for (int i = 0; i < rank.length; i++) {
+        //     if ("1".equals(rank[i])) {
+        //         rank[i] = "Gold Medal";
+        //     }
+        //     if ("2".equals(rank[i])) {
+        //         rank[i] = "Silver Medal";
+        //     }
+        //     if ("3".equals(rank[i])) {
+        //         rank[i] = "Bronze Medal";
+        //     }
+        // }
         return rank;
     }
 }
